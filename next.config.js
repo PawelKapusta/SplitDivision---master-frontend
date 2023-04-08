ca42b03b0142e79ba04d005c757cd7b9b074f429
PawelKapusta <pawel.kapusta@student.uj.edu.pl>
2023-04-08 13:34:36 +0200

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,41 @@
-const withPlugins = require("next-compose-plugins");
+/** @type {import('next').NextConfig} */
+const withImages = require("next-images");
+const withSass = require("@zeit/next-sass");
+const withReactSvg = require("next-react-svg");
+const withOffline = require("next-offline");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 });
+const withSourceMaps = require("@zeit/next-source-maps");
+const nextEnv = require("next-env");
+const dotenvLoad = require("dotenv-load");
 
-module.exports = withPlugins([[withBundleAnalyzer]], {});
+dotenvLoad();
+
+module.exports = withImages(
+    withSass(
+        withReactSvg(
+            withOffline(
+                withSourceMaps(
+                    nextEnv(
+                        withBundleAnalyzer({
+                            images: {
+                                domains: [""],
+                            },
+                            serverRuntimeConfig: {
+                                mySecret: "secret",
+                                secondSecret: process.env.SECOND_SECRET,
+                            },
+                            publicRuntimeConfig: {
+                                staticFolder: "/public",
+                            },
+                            webpack(config) {
+                                return config;
+                            },
+                        }),
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
