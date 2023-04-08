62756c3a686604efb8d6af112cf7095a4d6988a5
PawelKapusta <pawel.kapusta@student.uj.edu.pl>
2023-04-08 15:17:34 +0200

    Fix

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,3 @@
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
 };
-
-// import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-//
-// configure({ adapter: new Adapter() });
