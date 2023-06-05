3dd12562c6d0f80f2228cf986acf916fb8eabee9
PawelKapusta <pawel.kapusta@student.uj.edu.pl>
2023-06-05 19:10:49 +0200

    Update crypt-converter

--- src/components/converter/crypto-converter.tsx
+++ src/components/converter/crypto-converter.tsx
@@ -1,156 +1,155 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import axios from "axios";
-import {
-    ConverterContainer,
-    Title,
-    ConverterForm,
-    Input,
-    Select,
-    Button,
-    Result,
-    Icon,
-    SwapIcon,
-} from "./crypto-converter.styles";
+// Styled Components
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 100px;
+`;
 
-import {
-    faMoneyBill,
-    faCoins,
-    faExchangeAlt,
-} from "@fortawesome/free-solid-svg-icons";
+const Title = styled.h1`
+    font-size: 24px;
+    margin-bottom: 20px;
+`;
+
+const InputWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+    padding: 5px;
+    margin-right: 10px;
+`;
+
+const Button = styled.button`
+    padding: 5px 10px;
+    background-color: #2196f3;
+    color: #fff;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+`;
+
+const Result = styled.p`
+    font-size: 18px;
+`;
 
 const CryptoConverter = () => {
-    const [amount, setAmount] = useState("");
-    const [fromCurrency, setFromCurrency] = useState("");
-    const [toCurrency, setToCurrency] = useState("");
-    const [conversionRate, setConversionRate] = useState(0);
-    const [convertedAmount, setConvertedAmount] = useState("");
-    const [cryptocurrencies, setCryptocurrencies] = useState<string[]>([]);
+    const [amount, setAmount] = useState<number>(0);
+    const [fiatCurrency, setFiatCurrency] = useState<string>("usd");
+    const [cryptoCurrency, setCryptoCurrency] = useState<string>("bitcoin");
+    const [convertedAmount, setConvertedAmount] = useState<number | null>(null);
+    const [cryptoList, setCryptoList] = useState<any[]>([]);
+    const [fiatCurrencies, setFiatCurrencies] = useState<string[]>([]);
+    const [exchangeRates, setExchangeRates] = useState<any>({});
 
     useEffect(() => {
-        const fetchCryptocurrencies = async () => {
+        const fetchCryptoList = async () => {
             try {
-                const response = await axios.get<any[]>(
-                    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+                const response = await axios.get(
+                    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1",
                 );
-
-                const cryptoList = response.data.map((crypto) => crypto.id);
-                setCryptocurrencies(cryptoList);
-                console.log("cryptoList", cryptoList);
+                setCryptoList(response.data);
             } catch (error) {
-                console.error("Error fetching cryptocurrencies:", error);
+                console.error("Error fetching cryptocurrency list:", error);
             }
         };
 
-        fetchCryptocurrencies();
+        fetchCryptoList();
     }, []);
 
-    const handleConvert = () => {
-        const fetchConversionRate = async () => {
+    useEffect(() => {
+        const fetchFiatCurrencies = async () => {
             try {
-                const response = await axios.get<any[]>(
-                    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+                const response = await axios.get(
+                    "https://api.coingecko.com/api/v3/simple/supported_vs_currencies",
                 );
-                const cryptoList = response.data;
-
-                const fromCrypto = cryptoList.find(
-                    (crypto) => crypto.id === fromCurrency,
-                );
-                const toCrypto = cryptoList.find(
-                    (crypto) => crypto.id === toCurrency,
-                );
-
-                if (fromCrypto && toCrypto) {
-                    const fromPrice = fromCrypto.current_price;
-                    const toPrice = toCrypto.current_price;
-
-                    const conversionRate = fromPrice / toPrice;
-                    setConversionRate(conversionRate);
-                } else {
-                    setConversionRate(0);
-                }
+                const top100Fiats = response.data.slice(0, 100);
+                setFiatCurrencies(top100Fiats);
             } catch (error) {
-                console.error("Error fetching conversion rate:", error);
+                console.error("Error fetching fiat currencies:", error);
             }
         };
 
-        if (fromCurrency && toCurrency) {
-            fetchConversionRate();
+        fetchFiatCurrencies();
+    }, []);
+
+    useEffect(() => {
+        const fetchExchangeRates = async () => {
+            try {
+                const vsCurrencies = [
+                    ...fiatCurrencies,
+                    ...cryptoList.map((crypto) => crypto.id),
+                ];
+                const response = await axios.get(
+                    `https://api.coingecko.com/api/v3/simple/price?ids=${cryptoList
+                        .map((crypto) => crypto.id)
+                        .join(",")}&vs_currencies=${vsCurrencies.join(",")}`,
+                );
+                setExchangeRates(response.data);
+            } catch (error) {
+                console.error("Error fetching exchange rates:", error);
+            }
+        };
+
+        if (cryptoList.length > 0 && fiatCurrencies.length > 0) {
+            fetchExchangeRates();
         }
-    };
-    const convertCurrency = () => {
-        if (!fromCurrency || !toCurrency) {
-            return;
+    }, [cryptoList, fiatCurrencies]);
+
+    const handleConvert = () => {
+        const rate = exchangeRates[cryptoCurrency]?.[fiatCurrency];
+
+        if (rate) {
+            const converted = amount * rate;
+            setConvertedAmount(converted);
+        } else {
+            console.error(
+                `Exchange rate not found for ${cryptoCurrency}-${fiatCurrency}`,
+            );
         }
-
-        if (fromCurrency === toCurrency) {
-            // No conversion needed for same currencies
-            setConvertedAmount(amount);
-            return;
-        }
-
-        const amountToConvert = 1; // Since we're converting between cryptocurrencies, the amount is fixed at 1.
-
-        const fromCurrencyRate = conversionRate || 1;
-        const toCurrencyRate = conversionRate ? 1 / conversionRate : 1;
-
-        const convertedValue =
-            (amountToConvert * toCurrencyRate) / fromCurrencyRate;
-        setConvertedAmount(convertedValue.toFixed(8));
-    };
-
-    const swapCurrencies = () => {
-        setFromCurrency(toCurrency);
-        setToCurrency(fromCurrency);
-    };
-
-    const getCurrencyIcon = (currency: string) => {
-        return <Icon icon={faExchangeAlt} />;
     };
 
     return (
-        <ConverterContainer>
-            <Title>Cryptocurrency Converter</Title>
-            <ConverterForm>
+        <Container>
+            <Title>Crypto Converter</Title>
+            <InputWrapper>
                 <Input
                     type="number"
+                    step="any"
                     value={amount}
-                    onChange={(e) => setAmount(e.target.value)}
-                    placeholder="Enter amount"
+                    onChange={(e) => setAmount(parseFloat(e.target.value))}
                 />
-                <Select
-                    value={fromCurrency}
-                    onChange={(e) => setFromCurrency(e.target.value)}
+                <select
+                    value={fiatCurrency}
+                    onChange={(e) => setFiatCurrency(e.target.value)}
                 >
-                    <option value="">Select From Currency</option>
-                    {cryptocurrencies.map((crypto) => (
-                        <option key={crypto} value={crypto}>
-                            {crypto}
+                    <option value="usd">USD</option>
+                    {/* Add the other 99 most popular fiat currencies as options */}
+                </select>
+                <select
+                    value={cryptoCurrency}
+                    onChange={(e) => setCryptoCurrency(e.target.value)}
+                >
+                    {cryptoList.map((crypto) => (
+                        <option key={crypto.id} value={crypto.id}>
+                            {crypto.symbol.toUpperCase()}
                         </option>
                     ))}
-                </Select>
-                <SwapIcon icon={faExchangeAlt} onClick={swapCurrencies} />
-                <Select
-                    value={toCurrency}
-                    onChange={(e) => setToCurrency(e.target.value)}
-                >
-                    <option value="">Select To Currency</option>
-                    {cryptocurrencies.map((crypto) => (
-                        <option key={crypto} value={crypto}>
-                            {crypto}
-                        </option>
-                    ))}
-                </Select>
+                </select>
                 <Button onClick={handleConvert}>Convert</Button>
-            </ConverterForm>
-            <Result>
-                here
-                {amount && conversionRate ? (
-                    <>
-                        {amount} {fromCurrency} = {convertedAmount} {toCurrency}
-                    </>
-                ) : null}
-            </Result>
-        </ConverterContainer>
+            </InputWrapper>
+            {convertedAmount !== null && (
+                <Result>
+                    Converted Amount: {convertedAmount.toFixed(6)}{" "}
+                    {fiatCurrency.toUpperCase()}
+                </Result>
+            )}
+        </Container>
     );
 };
 
